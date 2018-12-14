d0e2ab51350a01ee2daa699790620d621681cede
ShinJungWoo <sjo506@naver.com>
2018-12-14 17:52:43 +0900

    beta0.2

--- test1.py
+++ test1.py
@@ -1,6 +1,7 @@
 from bs4 import BeautifulSoup
 import urllib.request
 import urllib.parse
+import re
 
 # JTBC Parser
 #http://fs.jtbc.joins.com//RSS/morningand.xml
@@ -13,13 +14,25 @@ class news_crawler:
         self.process()
 
     def process(self):
+
+
         self.response=urllib.request.urlopen(self.url)
         self.soup=BeautifulSoup(self.response,'html.parser')
-        self.result=self.soup.select("title")
+        self.result=self.soup.select("item")
 
-        for result in self.result:
-            self.full_string+=(result.string+"\n\n")
+        self.data = {}
+        # for result in self.result:
+        #     self.full_string+=(result.string+"\n\n")
 
+        #print(self.result)
+        p=re.compile("<link/>.*<description>")
+        for line in self.result:
+            title=str(line.find_all("title"))
+            link=p.findall(str(line))
+            title=title[8:-9]
+            link=link[0][7:-13]
+            self.data[str(title)]=link
 
+        #print(self.data)
 
 a=news_crawler()
--- test3.py
+++ test3.py
@@ -13,16 +13,24 @@ class send_email:
         self.my_email="sjo506@naver.com"
 
         self.msg_hander=test1.news_crawler()
-        self.contents=self.msg_hander.full_string
 
 
+    def body_process(self):
+        self.msg_hander.process()
+        self.contents=""
+        self.data=self.msg_hander.data
+
+        for key in self.data.keys():
+            tmp_data='<a href="%s">%s</a><br /><br />'%(self.data[key],key)
+            self.contents += tmp_data
+        #print(self.contents)
 
     def process(self):
         self.naver_server=smtplib.SMTP_SSL('smtp.naver.com',465)
         self.naver_server.login(self.id,self.pw)
 
     def sending(self,dst):
-        self.msg=MIMEText(self.contents,_charset='euc-kr')
+        self.msg=MIMEText(self.contents,_charset='euc-kr',_subtype='html')
         self.msg['Subject']=self.title
         self.msg['From']=self.my_email
         self.msg['To'] = dst
@@ -33,37 +41,50 @@ class send_email:
 
     def send(self,dst):
         self.process()
+        self.body_process()
         self.sending(dst)
         print("Send_done")
 
 
-def processing_send(*args):
+def processing_send(dir):
     run=True
     # dst='b7712983@naver.com'
     # dst='sjo506@naver.com'
-    set_time="0145"
+    # dst='haswari@naver.com'
+    set_time="1751"
     chk=True
+    try:
+        while run:
+            now=datetime.now()
+            #time="{:0>2}{:0>2}{:0>2}".format(now.hour,now.minute,now.second)
+            time = "{:0>2}{:0>2}".format(now.hour, now.minute)
+            time_title="{:0>4}-{:0>2}-{:0>2} News".format(now.year, now.month,now.day)
+            if time==set_time and chk==True:
+
+                dst=[]
+                with open(dir,'r') as f:
+                    lines=f.readlines()
+                    for line in lines:
+                        if line != "" and line != "\n":
+                            dst.append(line)
 
 
-    while run:
-        now=datetime.now()
-        #time="{:0>2}{:0>2}{:0>2}".format(now.hour,now.minute,now.second)
-        time = "{:0>2}{:0>2}".format(now.hour, now.minute)
-        time_title="{:0>4}-{:0>2}-{:0>2} News".format(now.year, now.month,now.day)
-        if time==set_time and chk==True:
+                handler=send_email()
+                handler.title=time_title
+                for mail in dst:
+                    handler.send(mail)
 
-            chk = False
-            handler=send_email()
-            handler.title=time_title
-            for dst in args:
-                handler.send(dst)
+                chk = False
+            elif time != set_time:
+                chk=True
 
-        elif time != set_time:
-            chk=True
-
-        sleep(5)
+            sleep(5)
+    except:
+        handler = send_email()
+        handler.title = "Error"
+        handler.send("sjo506@naver.com")
+        print("Error")
 
 
-
-processing_send('sjo506@naver.com','b7712983@naver.com')
+processing_send('data.txt')
 
